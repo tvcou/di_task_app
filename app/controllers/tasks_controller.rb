dada602010004785b6e0aa54bf8b98539cb6f970
tvcou <tvcou.zyx77@gmail.com>
2015-12-21 21:37:48 +0000

    "vi app/controllers/tasks_controller.rb"

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,11 @@
 class TasksController < ApplicationController
+  def create
+    @block = Block.find(params[:block_id])
+    @task = @block.tasks.create(task_params)
+    redirect_to block_path(@block.id)
+  end
+
+  private def task_params
+    params[:task].permit(:title)
+  end
 end
