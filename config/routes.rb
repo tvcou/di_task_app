227d8762569c90913fe83e24531d8efe288d7a27
tvcou <tvcou.zyx77@gmail.com>
2015-12-21 22:02:50 +0000

    "add toggle routing vi config/routes.rb"

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :blocks do
     resources :tasks, only: [:create, :destroy]
   end
+  
+  post '/blocks/:block_id/tasks/:id/toggle' => 'tasks#toggle'
+
   root 'blocks#index'
 end
